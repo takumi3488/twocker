0492ebf7cda7f51fd0ab38b48325f31c8f59fe36
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-02-25 01:36:09 +0900

    feat: implement HTTP methods (Post, Patch, Delete, Put) for TwockerClient

--- model/client.go
+++ model/client.go
@@ -34,3 +34,41 @@ func (c *TwockerClient) Get(url string) (*TwockerResponse, error) {
 
 	return NewTwockerResponse(resp.StatusCode, body), nil
 }
+
+func (c *TwockerClient) Post(url string, contentType string, body io.Reader) (*TwockerResponse, error) {
+	return command(c, http.MethodPost, url, contentType, body)
+}
+
+func (c *TwockerClient) Patch(url string, contentType string, body io.Reader) (*TwockerResponse, error) {
+	return command(c, http.MethodPatch, url, contentType, body)
+}
+
+func (c *TwockerClient) Delete(url string, contentType string, body io.Reader) (*TwockerResponse, error) {
+	return command(c, http.MethodDelete, url, contentType, body)
+}
+
+func (c *TwockerClient) Put(url string, contentType string, body io.Reader) (*TwockerResponse, error) {
+	return command(c, http.MethodPut, url, contentType, body)
+}
+
+func command(c *TwockerClient, method string, url string, contentType string, body io.Reader) (*TwockerResponse, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return &TwockerResponse{}, nil
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return &TwockerResponse{}, nil
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &TwockerResponse{}, nil
+	}
+
+	return NewTwockerResponse(resp.StatusCode, b), nil
+}
