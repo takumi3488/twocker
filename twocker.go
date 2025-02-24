cee879b62289ed297ca32cd4419e2cd871baa440
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-02-25 01:25:01 +0900

    feat: add type aliases for TwockerClient and TwockerResponse

--- twocker.go
+++ twocker.go
@@ -4,6 +4,9 @@ import (
 	"github.com/takumi3488/twocker/model"
 )
 
+type TwockerClient = model.TwockerClient
+type TwockerResponse = model.TwockerResponse
+
 func NewTwockerClient() *model.TwockerClient {
 	return model.NewTwockerClient()
 }
