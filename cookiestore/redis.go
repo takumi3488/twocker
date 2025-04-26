c47008550346b1369f6e61007125fb7bf086e860
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-04-27 04:04:35 +0900

    refactor: simplify cookie serialization and deserialization in RedisCookieStore

--- cookiestore/redis.go
+++ cookiestore/redis.go
@@ -3,10 +3,8 @@ package cookiestore
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/url"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -63,69 +61,23 @@ func (s *RedisCookieStore) Cookies(url *url.URL) []*http.Cookie {
 	return jsonToCookies(res)
 }
 
-type CookieJson struct {
-	Name       string        `json:"Name"`
-	Value      string        `json:"Value"`
-	Path       string        `json:"Path"`
-	Domain     string        `json:"Domain"`
-	Expires    string        `json:"Expires"`
-	RawExpires string        `json:"RawExpires"`
-	MaxAge     int           `json:"MaxAge"`
-	Secure     bool          `json:"Secure"`
-	HttpOnly   bool          `json:"HttpOnly"`
-	SameSite   http.SameSite `json:"SameSite"`
-	Raw        string        `json:"Raw"`
-}
-
 func cookiesToJson(c []*http.Cookie) string {
 	if len(c) == 0 {
 		return "[]"
 	}
-	s := "["
-	for _, cookie := range c {
-		s += "{"
-		s += "\"Name\": \"" + cookie.Name + "\","
-		s += "\"Value\": \"" + cookie.Value + "\","
-		s += "\"Path\": \"" + cookie.Path + "\","
-		s += "\"Domain\": \"" + cookie.Domain + "\","
-		s += "\"Expires\": \"" + cookie.Expires.Format(time.RFC3339) + "\","
-		s += "\"RawExpires\": \"" + cookie.RawExpires + "\","
-		s += "\"MaxAge\": " + fmt.Sprint(cookie.MaxAge) + ","
-		s += "\"Secure\": " + fmt.Sprint(cookie.Secure) + ","
-		s += "\"HttpOnly\": " + fmt.Sprint(cookie.HttpOnly) + ","
-		s += "\"SameSite\": " + fmt.Sprint(cookie.SameSite) + ","
-		s += "\"Raw\": \"" + cookie.Raw + "\""
-		s += "},"
-	}
-	s = s[:len(s)-1] + "]"
-	return s
-}
 
-func jsonToCookies(s string) []*http.Cookie {
-	var c []CookieJson
-	err := json.Unmarshal([]byte(s), &c)
+	cookiesJSON, err := json.Marshal(c)
 	if err != nil {
 		panic(err)
 	}
-	res := make([]*http.Cookie, len(c))
-	for i, cookie := range c {
-		expire, err := time.Parse(time.RFC3339, cookie.Expires)
-		if err != nil {
-			panic(err)
-		}
-		res[i] = &http.Cookie{
-			Name:       cookie.Name,
-			Value:      cookie.Value,
-			Path:       cookie.Path,
-			Domain:     cookie.Domain,
-			Expires:    expire,
-			RawExpires: cookie.RawExpires,
-			MaxAge:     cookie.MaxAge,
-			Secure:     cookie.Secure,
-			HttpOnly:   cookie.HttpOnly,
-			SameSite:   cookie.SameSite,
-			Raw:        cookie.Raw,
-		}
+	return string(cookiesJSON)
+}
+
+func jsonToCookies(s string) []*http.Cookie {
+	var cookies []*http.Cookie
+	err := json.Unmarshal([]byte(s), &cookies)
+	if err != nil {
+		panic(err)
 	}
-	return res
+	return cookies
 }
